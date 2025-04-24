e6657b0f6e38871e69e2be4d6d6482a937bf1c7b
NinjaFlor <andreiac.lima@outlook.com.br>
2025-04-24 15:21:04 -0300

    Correções

--- CURSOARRAYS/src/main.java
+++ CURSOARRAYS/src/main.java
@@ -1,20 +1,36 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class main {
+public class Main {
+    public static void main(String[] args) {
+        List<String> nomesPokemon = Arrays.asList("Pikachu", "Charmander", "Bulbassaur");
 
-	public static void main(String[] args) {
-		List<String>pokemon = new ArrayList<String>();
-		pokemon.add("Pikachu");
-		pokemon.add("Charmander");
-		pokemon.add("Bulbassaur");
+        List<List<String>> timePokemon = new ArrayList<>();
+        timePokemon.add(Arrays.asList("Pikachu", "M", "level 1"));
+        timePokemon.add(Arrays.asList("Chamander", "F", "level 4"));
 
-		pokemon.remove(0);
-		pokemon.remove(pokemon.size()-1);
+        List<String> pokemon = new ArrayList<>();
+        pokemon.add("Pikachu");
+        pokemon.add("Charmander");
+        pokemon.add("Bulbassaur");
 
+        pokemon.remove(0); // remove "Pikachu"
+        pokemon.remove(pokemon.size() - 1); // remove "Bulbassaur"
 
-		for (String p : pokemon) {
-			System.out.println(p);
-		}
-	}
+        System.out.println("Conteúdo da lista 'nomesPokemon':");
+        for (String nome : nomesPokemon) {
+            System.out.println(nome);
+        }
+
+        System.out.println("\nConteúdo da lista 'pokemon' após remoções:");
+        for (String p : pokemon) {
+            System.out.println(p);
+        }
+
+        System.out.println("\nConteúdo da lista 'timePokemon':");
+        for (List<String> pokeInfo : timePokemon) {
+            System.out.println(pokeInfo);
+        }
+    }
 }
