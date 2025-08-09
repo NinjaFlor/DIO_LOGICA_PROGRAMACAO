7918deb28a9c92c81477acb44b00d61e59fc3571
NinjaFlor <andreiac.lima@outlook.com.br>
2025-08-09 18:01:07 -0300

    Correção no desafio 2

--- src/desafio2/NovaClassificacaoHeroi.java
+++ src/desafio2/NovaClassificacaoHeroi.java
@@ -38,17 +38,17 @@ public class NovaClassificacaoHeroi {
         int saldo = vitorias - derrotas;
         String nivel;
 
-        if (saldo <= 10 ){
+        if (vitorias <= 10 ){
             nivel = "Ferro";
-        } else if (saldo >= 11 && vitorias <= 20) {
+        } else if (vitorias >= 11 && vitorias <= 20) {
             nivel = "Bronze";
-        } else if (saldo >= 21 && vitorias <= 50) {
+        } else if (vitorias >= 21 && vitorias <= 50) {
             nivel = "Prata";
-        } else if (saldo >= 51 && vitorias <= 80) {
+        } else if (vitorias >= 51 && vitorias <= 80) {
             nivel = "Ouro";
-        } else if (saldo >= 81 && vitorias <= 90) {
+        } else if (vitorias >= 81 && vitorias <= 90) {
             nivel = "Diamante";
-        } else if (saldo >= 91 && vitorias <= 100) {
+        } else if (vitorias >= 91 && vitorias <= 100) {
             nivel = "Lendário";
         } else {
             nivel = "Imortal";
